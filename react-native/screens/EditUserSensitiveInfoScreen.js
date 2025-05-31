f410914bc4ba409965989434464a91933b8dcdf6
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-05-31 21:11:15 +0200

    created EditUserScreen and refactored back-end logic

--- react-native/screens/EditUserSensitiveInfoScreen.js
+++ react-native/screens/EditUserSensitiveInfoScreen.js
@@ -1,26 +1,94 @@
-import React, {useState, useEffect} from 'react'
-import {Button, View, Text} from 'react-native'
-import { getUserSensitiveInfo } from '../src/api/userApi'
-const EditUserSensitiveInfoScreen = ({route}) => {
-    const [user, setUser] = useState([])
-    const { id } = route.params
-    useEffect(() => {
-        const fetchUser = async () => {
-            try{
-                const response = await getUserSensitiveInfo(id)
-                setUser(response)
-            } catch { throw new Error('Failed to get user')}
-        }
-        fetchUser()
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { styles } from '../styles/mainStyles'
+import UserEdit from '../components/UserEdit'
+import NavigatePanel from '../components/NavigatePanel'
+import { getUserSensitiveInfo, editUserSensitiveInfo } from '../src/api/userApi'
+
+const EditUserSensitiveInfoScreen = ({ navigation, route }) => {
+  const [user, setUser] = useState({
+    id: '',
+    userName: '',
+    email: '',
+    role: '',
+    password: '',
+  })
+  const { id } = route.params
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await getUserSensitiveInfo(id)
+        console.log(response)
+        setUser({
+          id: response.id || '',
+          userName: response.userName || '',
+          email: response.email || '',
+          role: response.role || '',
+          password: response.password || ''
+        })
+      } catch {
+        throw new Error('Failed to get user')
+      }
     }
-    ,[])
-    return (
-        <View>
-            <Text>{user.userName}</Text>
-            <Text>{user.email}</Text>
-            <Text>{user.role}</Text>
-            <Text>{user.password}</Text>
-        </View>
-    )
+    fetchUser()
+  }, [])
+  console.log(user)
+  const handleSaveInfo = async (id, data) => {
+    try {
+      const response = await editUserSensitiveInfo(id, data)
+      navigation.navigate('UserList')
+      return response
+    } catch {
+      alert('Failed to edit user')
+    }
+  }
+
+  return (
+    <View style={styles.containerUserListScreen}>
+      <View style={styles.containerNameScreen}>
+        <Text style={styles.textNameScreen}>Edit</Text>
+      </View>
+      <View style={styles.containerEditUser}>
+        <UserEdit
+          text="user name"
+          editText={user.userName}
+          onChangeText={(newText) => setUser({ ...user, userName: newText })}
+        />
+        <UserEdit
+          text="email"
+          editText={user.email}
+          onChangeText={(newText) => setUser({ ...user, email: newText })}
+        />
+        <UserEdit
+          text="role"
+          editText={user.role}
+          onChangeText={(newText) => setUser({ ...user, role: newText })}
+        />
+        <UserEdit
+          text="password"
+          editText={user.password}
+          onChangeText={(newText) => setUser({ ...user, password: newText })}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            const { id, ...userData } = user
+            handleSaveInfo(id, userData)
+          }}
+        >
+          <View style={styles.containerSaveButton}>
+            <Text style={styles.textSaveButton}>Save</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <NavigatePanel
+        onPressList={() => {
+          navigation.navigate('UserList')
+        }}
+        onPressAdd={() => {
+          navigation.navigate('AddUser')
+        }}
+      />
+    </View>
+  )
 }
 export default EditUserSensitiveInfoScreen
