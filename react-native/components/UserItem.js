a45d05574ac06f3d9189567a41f80ce509b01a82
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-05-31 21:14:27 +0200

    Fix UI UserList(UserItem)

--- react-native/components/UserItem.js
+++ react-native/components/UserItem.js
@@ -2,24 +2,33 @@ import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import { styles } from '../styles/mainStyles'
 
-const UserItem = ({user, onShow, onEdit, onDelete}) => {
-    const { userName, role } = user
-    return (
-        <View style={styles.containerUserComponent}>
-            <Text>{userName}</Text>
-            <Text>{role}</Text>
-            <View style={styles.containerUserFunctions}>
-                <TouchableOpacity onPress={onShow}>
-                    <Image source={require("../assets/images/workLogsLogo.svg")}/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={onEdit}>
-                    <Image source={require("../assets/images/editUserLogo.svg")}/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={onDelete}>
-                    <Image source={require("../assets/images/deleteUserLogo.svg")}/>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+const UserItem = ({ user, onShow, onEdit, onDelete }) => {
+  const { userName, role } = user
+  return (
+    <View style={styles.containerUserComponent}>
+      <View style={styles.wrapperName}>
+        <Text style={styles.nameText}>{userName}</Text>
+      </View>
+
+      <View style={styles.wrapperRole}>
+        <Text style={styles.roleText}>{role}</Text>
+      </View>
+
+      <View style={styles.containerUserFunctions}>
+        <TouchableOpacity onPress={onShow}>
+          <Image source={require('../assets/images/workLogsLogo.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onEdit}>
+          <Image
+            source={require('../assets/images/editUserLogo.png')}
+            style={{ marginHorizontal: 15 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onDelete}>
+          <Image source={require('../assets/images/deleteUserLogo.png')} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
 }
 export default UserItem
