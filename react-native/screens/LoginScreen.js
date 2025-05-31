2a254d9ee1dcf3633672a9233e8e1958ba6564c8
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-05-31 21:23:51 +0200

    Changed UI in Auth`s part added new component  AuthAskText

--- react-native/screens/LoginScreen.js
+++ react-native/screens/LoginScreen.js
@@ -1,37 +1,39 @@
-import React, {useState, useContext} from 'react'
-import { View, Keyboard, TouchableWithoutFeedback, KeyboardAvoidingView, Platform } from 'react-native'
+import React, { useState, useContext } from 'react'
+import {
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native'
 import { styles } from '../styles/authStyles'
 import AuthField from '../components/AuthFields'
 import AuthSubmitButton from '../components/AuthSubmitButton'
-import {loginAdmin} from '../src/api/adminApi'
+import AuthAskText from '../components/AuthAskText'
+import { loginAdmin } from '../src/api/adminApi'
 import { saveToken } from '../utils/tokenStorage'
 import { AuthContext } from '../context/AuthContext'
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   const [userName, setUserName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const {logout, login} = useContext(AuthContext)
+  const { logout, login } = useContext(AuthContext)
   const handleLogin = async () => {
-
     if (!userName || !email || !password) {
       alert('Please fill in all fields')
       return
     }
 
     try {
-
-      const {token} = await loginAdmin(userName, email, password)
+      const { token } = await loginAdmin(userName, email, password)
       await saveToken(token)
       login()
-  
-
-    } catch (error){
+    } catch (error) {
       if (error.message === 'Unauthorized. Please check your credentials') {
         logout()
         alert('Unauthorized. Please check your credentials')
-      }
-      else if (error.message === 'Login failed. Please try again') {
+      } else if (error.message === 'Login failed. Please try again') {
         alert('Login failed. Please try again')
       } else if (error.message === 'Admin not found') {
         alert('Admin not found')
@@ -42,25 +44,38 @@ const LoginScreen = ({navigation}) => {
   }
 
   return (
-  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <View style={styles.authContainer}>
+          <View style={{ marginTop: 160 }}>
+            <AuthField
+              label="user name"
+              value={userName}
+              onChangeText={setUserName}
+            />
+          </View>
+          <AuthField label="email" value={email} onChangeText={setEmail} />
+          <AuthField
+            label="password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true}
+          />
 
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <View style={{ ...styles.authContainer, height: 400, }}>
-            <View>
-              <View style={{ marginTop: 40 }}>
-                <AuthField label="UserName" value={userName} onChangeText={setUserName}/>
-              </View>
-              <AuthField label="Email" value={email} onChangeText={setEmail} />
-              <AuthField label="Password" value={password} onChangeText={setPassword} secureTextEntry={true}/>
-            </View>
-          <AuthSubmitButton onPress={handleLogin}/>
+          <AuthSubmitButton onPress={handleLogin} text="Sign In" />
+          <AuthAskText
+            onPress={() => {
+              navigation.navigate('Register')
+            }}
+            mainText="Don`t have an account?"
+            buttonText="Sign Up"
+          />
         </View>
-      </View>
-    </KeyboardAvoidingView>
-  </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   )
 }
 export default LoginScreen
